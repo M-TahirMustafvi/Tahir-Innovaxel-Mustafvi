0a6e69433124cb7393f23549fa57d94d0ffc7492
= <=>
2025-03-18 20:26:29 +0500

    Managed ShortURL Generator

--- Controller/urlController.js
+++ Controller/urlController.js
@@ -1,19 +1,5 @@
 const URLs = require('../Models/urlModel');
-const crypto = require('crypto');
-
-function generateUniqueString() {
-    // Generate 3 random bytes (24 bits of entropy)
-    const randomBytes = crypto.randomBytes(3);
-    // Convert to a hexadecimal string and then to an integer
-    const num = parseInt(randomBytes.toString('hex'), 16);
-    // Convert to base36 and pad/truncate to 6 characters
-    let base36 = num.toString(36).toUpperCase();
-    // Ensure it’s 6 characters (pad with zeros if needed, or slice if too long)
-    return base36.padStart(6, '0').substring(0, 6);
-  }
-
-
-
+const shortURL = require('./shortUrl');
 
 //Creates Short Url and stores it in DB
 exports.createShortUrl = async(req, res) => {
@@ -21,7 +7,7 @@ exports.createShortUrl = async(req, res) => {
         const {url} = req.body;
         if(!url) return res.status(400).json({error: 'URL required.'});
 
-        const shortCode = `https://www.${generateUniqueString()}.com`;
+        const shortCode = `https://www.${shortURL}.com`;
         const newURL = await URLs.create({url, shortCode});
         
         const { accessCount, ...responseData } = newURL.toJSON();
