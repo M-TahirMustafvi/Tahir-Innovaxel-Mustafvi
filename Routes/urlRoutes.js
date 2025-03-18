5e84a60d24528c184eee7652711dd5aa224e84cf
= <=>
2025-03-18 20:00:38 +0500

    GET/Retrieve Endpoint

--- Routes/urlRoutes.js
+++ Routes/urlRoutes.js
@@ -6,6 +6,8 @@ const urlController = require('../Controller/urlController');
 // Posts a new URL, assigns it a shorten URL, and stores it, in DB.
 router.post('/', urlController.createShortUrl);
 
+//Gets a URL object form db based on short url
+router.get('/:shortCode', urlController.getLongUrl);
 
 
 module.exports = router;
